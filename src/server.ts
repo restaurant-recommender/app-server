5333753fe154ed8186ae950869b59d7afb1caba4
Chanchana Wicha <chanchana@wongnai.com>
2020-10-05 00:34:33 +0700

    add gcloud

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,5 @@ import env from "./config/environments"
 
 app.listen(env.port, () => {
     console.log(`Express server listening on port: ${env.port}`)
+    console.log(`Mongodb: ${env.mongoURL}`)
 })
